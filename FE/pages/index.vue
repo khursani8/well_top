fa34083993bafb2e16bb3e92a34e0d803a50b2b6
Sani <khursani8@gmail.com>
2019-02-24 06:31:14 +0800

    done fe

--- FE/pages/index.vue
+++ FE/pages/index.vue
@@ -1,8 +1,9 @@
 <template>
   <section class="container">
-    <el-container>
+    <el-container direction="vertical">
+      <div>Automatic Selection Top Formation Module</div>
       <el-main>
-        <el-container direction="horizontal" height="30%" id="top">
+        <el-container direction="horizontal" height="29%" id="top">
           <el-container direction="vertical">
             <el-aside width="100%">
               <el-upload
@@ -12,6 +13,7 @@
                 class="upload-demo"
                 action="https://jsonplaceholder.typicode.com/posts/"
                 :auto-upload="false"
+                multiple
               >
                 <el-button slot="trigger" size="small" type="primary">select file</el-button>
               </el-upload>
@@ -19,26 +21,44 @@
             <div class="well">
               <el-checkbox-group v-model="checkList" style="overflow-y:scroll">
                 <el-container direction="vertical">
-                  <el-checkbox v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox>
+                  <el-checkbox
+                    v-for="city in wellNames"
+                    :label="city"
+                    :key="triggerReRender+city"
+                  >{{city}}</el-checkbox>
                 </el-container>
               </el-checkbox-group>
-              <el-button type="primary">Generate Log</el-button>
+              <el-button type="primary" @click="generate">Generate Log</el-button>
             </div>
           </el-container>
 
-          <el-aside width="60%" height="70%">
-            <div id="gmap-container" height="400px"/>
+          <el-aside width="80%" height="70%">
+            <div id="gmap-container" height="710px"/>
           </el-aside>
         </el-container>
 
         <!-- testadadadsd -->
         <el-container direction="horizontal" style="text-align:center" id="bottom">
           <div width="100px" id="fullscreen">
-            <el-checkbox v-model="checked">
-              <input :disabled="!checked" placeholder="GR CUTOFF">
-            </el-checkbox>
+            <el-tooltip
+              class="item"
+              effect="dark"
+              content="Gamma Ray Cut off"
+              placement="top-start"
+            >
+              <el-checkbox v-model="checked">
+                <el-input-number
+                  size="mini"
+                  v-model="cutOff"
+                  @change="handleChange"
+                  :min="0"
+                  :max="200"
+                ></el-input-number>
+                <!-- <input :disabled="!checked" placeholder="GR CUTOFF" v-model="cutOff"> -->
+              </el-checkbox>
+            </el-tooltip>
           </div>
-          <el-main>Correlation Panel</el-main>
+          <el-main id="correlation">Correlation Panel</el-main>
           <input id="button" type="button" value="FULL" @click="full">
         </el-container>
       </el-main>
@@ -47,23 +67,26 @@
 </template>
 
 <script>
-const cityOptions = ['WellA', 'WellB', 'WellC', 'WellD']
+import Papa from 'papaparse'
 export default {
   components: {},
   data() {
     return {
+      cutOff: 120,
+      triggerReRender: Math.random(),
+      files: [],
+      wellNames: [],
       fileList3: [],
       checked: false,
       checkList: [],
       checkAll: false,
-      checkedCities: ['WellA', 'WellC'],
-      cities: cityOptions,
+      checkedCities: [],
       isIndeterminate: true,
       vueGMap: null,
       // Basically, making an anon function so that Vue
       // doesn't try to access google.maps until it's actually required
       globalOptions: () => ({
-        zoom: 4,
+        zoom: 15,
         center: { lat: -33, lng: 151 },
         mapTypeControl: true,
         mapTypeControlOptions: {
@@ -80,9 +103,53 @@ export default {
     )
   },
   methods: {
-    handleChange(file, fileList) {
+    generate() {
+      const payload = {
+        files: this.files.filter(el => this.checkList.includes(el.well)),
+        GR_CUTOFF: this.GR_CUTOFF
+      }
+      this.$axios.post('/endpoint', payload)
       debugger
-      this.fileList3 = fileList.slice(-3)
+    },
+    handleChange(file, fileList) {
+      const f = file.raw
+      const that = this
+      Papa.parse(f, {
+        header: true,
+        complete: function(results) {
+          setTimeout(() => {
+            const { lat, lon, well } = results.data[0]
+            const myLatlng = { lat: +lat, lng: +lon }
+            if (that.wellNames.length === 0) {
+              const gmap = document.getElementById('gmap-container')
+              const test = that.globalOptions()
+              test.center = myLatlng
+              that.vueGMap = new google.maps.Map(gmap, test)
+              that.wellNames = []
+            }
+            that.wellNames.push(well)
+            // console.log('before', that.triggerReRender)
+            // that.triggerReRender = Math.random()
+            // console.log('after', that.triggerReRender)
+            // console.log('well', that.wellNames)
+            console.log(lat, lon, well)
+            // const test = new google.maps.Marker({
+            //   // position: { lat: -33, lng: 151 },
+            //   position: myLatlng,
+            //   map: that.vueGMap,
+            //   title: 'Hello World!'
+            // })
+            const marker = new google.maps.Marker({
+              position: myLatlng,
+              title: well
+            })
+
+            // To add the marker to the map, call setMap();
+            marker.setMap(that.vueGMap)
+            that.files.push({ well, file: results.data })
+          }, 0)
+        }
+      })
     },
     full() {
       const display = document.getElementById('top').style.display
@@ -104,38 +171,35 @@ export default {
     },
     initGoogleMaps() {
       // You could do this as an easier alternative
-      const localOptions = {
-        zoom: 4,
-        center: { lat: -33, lng: 151 },
-        mapTypeControl: true,
-        mapTypeControlOptions: {
-          style: () => window.google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-          mapTypeIds: ['roadmap', 'terrain']
-        }
-      }
+      const myLatlng = { lat: -33, lng: 151 }
+      // const localOptions = {
+      //   zoom: 15,
+      //   center: myLatlng,
+      //   mapTypeControl: true,
+      //   mapTypeControlOptions: {
+      //     style: () => window.google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+      //     mapTypeIds: ['roadmap', 'terrain']
+      //   }
+      // }
       const gmap = document.getElementById('gmap-container')
       this.vueGMap = new google.maps.Map(gmap, this.globalOptions())
-      gmap.style.height = '500px'
+      gmap.style.height = '710px'
       console.log(this.vueGMap)
     },
     googleMapsFailedToLoad() {
       this.vueGMap = 'Error occurred'
-    },
-    handleCheckAllChange(val) {
-      this.checkedCities = val ? cityOptions : []
-      this.isIndeterminate = false
-    },
-    handleCheckedCitiesChange(value) {
-      const checkedCount = value.length
-      this.checkAll = checkedCount === this.cities.length
-      this.isIndeterminate =
-        checkedCount > 0 && checkedCount < this.cities.length
     }
   }
 }
 </script>
 
 <style>
+div.el-checkbox-group {
+  height: 200px;
+}
+.el-upload-list.el-upload-list--text {
+  height: 200px;
+}
 #fullscreen {
   position: relative;
   top: 0;
@@ -167,11 +231,19 @@ div.el-upload el-upload--text {
   line-height: 200px;
 }
 
+#bottom {
+  background-color: #d1dce6;
+}
+
 .el-main {
   background-color: #e9eef3;
   color: #333;
   text-align: center;
   line-height: 160px;
+}
+
+#correlation {
+  background-color: #d1dce6;
 }
 
 body > .el-container {
